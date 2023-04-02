83116898c62c043d1df54d33aabb0457e7e06ba6
Arnaud Wanet <andrewwanet9@gmail.com>
2023-04-02 17:03:47 +0100

    trade store added

---
+++ store/mysql/trade_store.go
@@ -0,0 +1,30 @@
+package mysql 
+
+import (
+	"github.com/irononet/go-exchange/entities" 
+	"gorm.io/gorm"
+)
+
+func (s *Store) GetLastTradeByProduct(productId string) (*entities.Trade, error){
+	var trade entities.Trade  
+	err := s.db.Where("product_id=?", productId).Order("id DESC").Limit(1).Find(&trade).Error 
+	if err == gorm.ErrRecordNotFound{
+		return nil, nil 
+	}
+	return &trade, err 
+}
+
+func (s *Store) GetTradesByProductId(productId string, limit int) ([]*entities.Trade, error){
+	db := s.db.Where("product_id=?", productId).Order("id DESC").Limit(limit) 
+	var trades []*entities.Trade 
+	err := db.Find(&trades).Error 
+	return trades, err 
+
+}
+
+func (s *Store) AddTrades(trades []*entities.Trade) error{
+	if len(trades) == 0{
+		return nil 
+	}
+	return s.db.Create(trades).Error 
+}
