ed7548283ba0c1ae32a1d36977ca36a5503da748
Arnaud Wanet <andrewwanet9@gmail.com>
2023-04-05 15:08:55 +0100

    wallet service still WIP

---
+++ services/wallet_service.go
@@ -0,0 +1,10 @@
+package services
+
+import (
+	"github.com/shopspring/decimal"
+	
+) 
+
+func createWithdrawTransaction(userId int64, currency string, amount decimal.Decimal, address string){
+	
+}
