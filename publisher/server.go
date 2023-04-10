c75c8eaeeb2b9c9fadd061746a6f8a1f4aefdd19
Arnaud Wanet <andrewwanet9@gmail.com>
2023-04-10 23:37:03 +0100

    added the websocket server type

---
+++ publisher/server.go
@@ -0,0 +1,51 @@
+package publisher 
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket" 
+	"github.com/siddontang/go-log/log" 
+	"io/ioutil" 
+	"net/http"
+)
+
+
+type Server struct{
+	Addr string 
+	Path string 
+	Sub *Subscription 
+}
+
+func NewServer(addr, path string, sub *Subscription) *Server{
+	return &Server{
+		Addr: addr, 
+		Path: path, 
+		Sub: sub, 
+	}
+}
+
+func (s *Server) Ws(c *gin.Context){
+	upgrader := websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool{
+			return true 
+		},
+	}
+
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil) 
+	if err != nil{
+		log.Error(err) 
+	}
+
+	NewClient(conn, s.Sub).StartServe()
+}
+
+func (s *Server) Run(){
+	gin.SetMode(gin.ReleaseMode) 
+	gin.DefaultWriter = ioutil.Discard 
+
+	r := gin.Default() 
+	r.GET(s.Path, s.Ws) 
+	err := r.Run(s.Addr) 
+	if err != nil{
+		panic(err)
+	}
+}
