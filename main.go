0f07127bdb67dff279391012bf741f5c827492ce
Arnaud Wanet <andrewwanet9@gmail.com>
2023-04-19 13:48:03 +0100

    main routine added

--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"net/http"
+	_ "net/http/pprof"
+	"strconv"
+
+	"github.com/irononet/go-exchange/conf"
+	"github.com/irononet/go-exchange/events"
+	"github.com/irononet/go-exchange/matching"
+	"github.com/irononet/go-exchange/publisher"
+	"github.com/irononet/go-exchange/restapi"
+	"github.com/irononet/go-exchange/service"
+	"github.com/irononet/go-exchange/worker"
+	"github.com/prometheus/common/log"
+)
+
 func main() {
 
+	gexConfig := conf.GetConfig() 
+
+	go func(){
+		log.Info(http.ListenAndServe("localhost:6060", nil))
+	}() 
+
+	go events.NewBinLogStream().Start() 
+
+	matching.StartEngine() 
+
+	publisher.StartServer() 
+
+	worker.NewFillExecutor().Start() 
+
+	worker.NewBillExecutor().Start() 
+
+	products, err := service.GetProducts() 
+	if err != nil{
+		panic(err) 
+	}
+
+	for _, product := range products{
+		productIdStr := strconv.Itoa(int(product.ID))
+		worker.NewTickMaker(productIdStr, matching.NewKafkaLogReader("tickMaker", productIdStr, gexConfig.Kafka.Brokers )).Start() 
+		worker.NewFillMaker(matching.NewKafkaLogReader("fillMaker", productIdStr, gexConfig.Kafka.Brokers)).Start() 
+		worker.NewTradeMaker(matching.NewKafkaLogReader("tradeMaker", productIdStr, gexConfig.Kafka.Brokers)).Start() 
+
+	}
+
+	restapi.StartServer() 
+
+	select{}
 }
